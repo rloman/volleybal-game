0f10ee9e7e4f216443c1926535048739672e4557
Raymond Loman <raymond@carpago.nl>
2018-05-08 09:21:52 +0200

    init

---
+++ src/main/java/nl/capgemini/volleybal/volleybalgame/VolleybalGameApplication.java
@@ -0,0 +1,12 @@
+package nl.capgemini.volleybal.volleybalgame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VolleybalGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VolleybalGameApplication.class, args);
+	}
+}
